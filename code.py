9acd1fb7ae0a71869e67f66b93fcc38ea0899ee3
mcMineyC <jedi.master@falconhosting.net>
2025-05-09 18:49:06 -0700

    Deploy on Fri May  9 18:49:06 PDT 2025

--- code.py
+++ code.py
@@ -64,7 +64,7 @@ display.auto_refresh = False # Don't show bg yet
 splash = displayio.Group()
 display.root_group = splash
 
-dht = adafruit_dht.DHT22(board.GP31)
+dht = adafruit_dht.DHT22(board.GP26)
 
 # Load fonts
 font_big = False
